5faf1a7326c7e8436e5e2e18ea9cd6d13cb4c88a
Heather You <heatherhyou@gmail.com>
2017-05-23 21:40:08 +0000

    Hard coded #list_categories method to test #call method

--- lib/nyt_bestseller_cli_app.rb
+++ lib/nyt_bestseller_cli_app.rb
@@ -1,6 +1,2 @@
 require_relative "./nyt_bestseller_cli_app/version"
 require_relative "./nyt_bestseller_cli_app/cli"
-
-module NytBestsellerCliApp
-  # Your code goes here...
-end
--- lib/nyt_bestseller_cli_app/cli.rb
+++ lib/nyt_bestseller_cli_app/cli.rb
@@ -3,6 +3,17 @@ class NytBestsellerCliApp::CLI
 
   def call
     puts "This week's Best Selling Books:"
+    list_categories
+  end
+
+  def list_categories
+    puts <<-DOC.gsub /^\s*/, ''
+      1. Combined Print & E-Book Fiction
+      2. Hardcover Fiction
+      3. Combined Print & E-Book Nonfiction
+      4. Hardcover Nonfiction
+      5. Paperback Nonfiction
+    DOC
   end
 
 end
