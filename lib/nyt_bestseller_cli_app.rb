89c73cbf82252c57e0f2b29b2963a82e596d7aa1
Heather You <heatherhyou@gmail.com>
2017-05-23 20:00:40 +0000

    Added CLI controller

--- lib/nyt_bestseller_cli_app.rb
+++ lib/nyt_bestseller_cli_app.rb
@@ -1,4 +1,5 @@
-require "nyt_bestseller_cli_app/version"
+require_relative "./nyt_bestseller_cli_app/version"
+require_relative "./nyt_bestseller_cli_app/cli"
 
 module NytBestsellerCliApp
   # Your code goes here...
---
+++ lib/nyt_bestseller_cli_app/cli.rb
@@ -0,0 +1,8 @@
+#Our CLI Controller
+class NytBestsellerCliApp::CLI
+
+  def call
+    puts "This week's Best Selling Books:"
+  end
+
+end
