3f40e256c7bc9c52297c1261ef781d461dc41ff8
Heather You <heatherhyou@gmail.com>
2017-05-24 03:43:53 +0000

    Started scraping

--- lib/nyt_bestseller_cli_app/category.rb
+++ lib/nyt_bestseller_cli_app/category.rb
@@ -2,34 +2,45 @@ class NytBestsellerCliApp::Category
   attr_accessor :name, :url
 
   def self.all
-    #I should return a bunch of instances of Category
+    #It should return a bunch of instances of Category
     self.scrape_categories
   end
 
   def self.scrape_categories
     categories = []
 
+    categories << self.scrape_cat1
+    categories << self.scrape_cat2
+    categories << self.scrape_cat3
+    categories << self.scrape_cat4
+
     category_1 = self.new
-    category_1.name = "Combined Print & E-Book Fiction"
-    category_1.url = "https://www.nytimes.com/books/best-sellers/combined-print-and-e-book-fiction/"
+    category_1.name = "Hardcover Fiction"
+    category_1.url = "http://www.barnesandnoble.com/b/new-york-times-bestsellers-hardcover-fiction/_/N-1p3r"
 
     category_2 = self.new
-    category_2.name = "Hardcover Fiction"
-    category_2.url = "https://www.nytimes.com/books/best-sellers/hardcover-fiction/"
+    category_2.name = "Hardcover Nonfiction"
+    category_2.url = "http://www.barnesandnoble.com/b/new-york-times-bestsellers-hardcover-nonfiction/_/N-1p5q"
 
     category_3 = self.new
-    category_3.name = "Combined Print & E-Book Nonfiction"
-    category_3.url = "https://www.nytimes.com/books/best-sellers/combined-print-and-e-book-nonfiction/"
+    category_3.name = "Paperback Fiction"
+    category_3.url = "http://www.barnesandnoble.com/b/new-york-times-bestsellers-trade-paperback-fiction/_/N-1p3v"
 
     category_4 = self.new
-    category_4.name = "Hardcover Nonfiction"
-    category_4.url = "https://www.nytimes.com/books/best-sellers/hardcover-nonfiction/"
+    category_4.name = "Paperback Nonfiction"
+    category_4.url = "http://www.barnesandnoble.com/b/new-york-times-bestsellers-paperback-nonfiction/_/N-1p3u"
 
-    category_5 = self.new
-    category_5.name = "Paperback Nonfiction"
-    category_5.url = "https://www.nytimes.com/books/best-sellers/paperback-nonfiction"
-
-    [category_1, category_2, category_3, category_4, category_5]
+    [category_1, category_2, category_3, category_4]
   end
 
+  def self.scrape_cat1
+    doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/new-york-times-bestsellers-hardcover-fiction/_/N-1p3r"))
+    binding.pry
+    name = doc.css(".html-embed-container").first.text
+    list = doc.css(".resultsListContainer").css('ul')[1]
+    list.css('li').each.with_index(1) |book, i|
+      puts "#{i}. #{book.text}"
+    end
+
+  end
 end
