983ac0b41df3e1713e33e452379383ba41ca4a59
hyoyou <heatherhyou@gmail.com>
2017-05-24 18:57:20 -0400

    Modified CLI from if/else to case statement

--- lib/nyt_bestseller/cli.rb
+++ lib/nyt_bestseller/cli.rb
@@ -2,29 +2,44 @@
 class NytBestseller::CLI
 
   def call
+    puts "Welcome to the NYTimes Bestseller List"
+    puts ""
+    puts "Which format and genre of books would you like to see the bestseller list for?"
     list_categories
     menu
   end
 
   def list_categories
-    puts "This week's Best Selling Books:"
+    puts ""
     @categories = NytBestseller::Category.all
     @categories.each.with_index(1) do |category, i|
-      puts "#{i}. #{category.name}"
+      puts "     #{i}. #{category.name}"
     end
   end
 
   def menu
     input = nil
     while input != "exit"
-      puts "Enter the number of the category you would like to see the top 15 books for or type list to see the categories again or type exit:"
+      puts ""
+      puts "Enter the number of the category you would like to see or type exit."
+      puts "You may also type list to see the list again:"
       input = gets.strip.downcase
-      if input.to_i > 0
+      case input
+      when "1"
         categories = @categories[input.to_i-1]
         puts "#{categories.name}"
-      elsif input == "list"
+      when "2"
+        categories = @categories[input.to_i-1]
+        puts "#{categories.name}"
+      when "3"
+        categories = @categories[input.to_i-1]
+        puts "#{categories.name}"
+      when "4"
+        categories = @categories[input.to_i-1]
+        puts "#{categories.name}"
+      when "list"
         list_categories
-      elsif input == "exit"
+      when "exit"
         goodbye
       else
         puts "Please check your input!"
