e75af9b34495bd1036f85db4b9b3970a58573408
hyoyou <heatherhyou@gmail.com>
2017-05-25 17:07:19 -0400

    Made new methods for getting user input and scraping individual books

--- lib/nyt_bestseller/scraper.rb
+++ lib/nyt_bestseller/scraper.rb
@@ -1,5 +1,5 @@
 class NytBestseller::Scraper
-  attr_accessor :name, :url
+  attr_accessor :input, :book, :url, :doc
 
   def self.all
     #It should return a bunch of instances of Category called in CLI #list_categories
@@ -27,115 +27,67 @@ class NytBestseller::Scraper
 
     i = 1
     while i < 11
-      listview = doc.css("#listView > li:nth-child(#{i}) > ul > li > div.product-info")
+      listview = @doc.css("#listView > li:nth-child(#{i}) > ul > li > div.product-info")
       puts "     #{i}. " + "#{listview.css("p.product-info-title > a").text} by: " + "#{listview.css("span > a").first.text}"
       i += 1
     end
   end
 
-  def self.scrape_cat1 #Hardcover Fiction
-    doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/new-york-times-bestsellers-hardcover-fiction/_/N-1p3r"))
-    top_ten(doc)
+  def self.get_input
     puts ""
     puts "Please type the number of the book you would like more information on:"
-    input = gets.strip
-    book = input.to_i
+    @input = gets.strip
+    @book = @input.to_i
+  end
 
-    url = "http://www.barnesandnoble.com#{doc.css("#listView > li:nth-child(#{book}) > ul > li > div.product-image > a:nth-child(1)").attribute("href").value}"
+  def self.get_book
+    url = "http://www.barnesandnoble.com#{@doc.css("#listView > li:nth-child(#{@book}) > ul > li > div.product-image > a:nth-child(1)").attribute("href").value}"
     book_info = Nokogiri::HTML(open("#{url}"))
     title = book_info.css("#prodSummary > h1").text
-    author = book_info.css("#prodSummary > span > a").text
-    summary = book_info.css("#productInfoOverview > div > div > p:nth-child(3)").text
+    author = book_info.css("#prodSummary > span > a").first.text
+    summary = book_info.css("#productInfoOverview > div > div > p:nth-child(3)").text.strip
     puts ""
     puts "-------------------------------------------------------"
     puts "TITLE:       #{title}"
     puts "AUTHOR:      #{author}"
     puts ""
     puts "SYNOPSIS:    #{summary}"
+  end
+
+
+  def self.scrape_cat1 #Hardcover Fiction
+    @doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/new-york-times-bestsellers-hardcover-fiction/_/N-1p3r"))
+    top_ten(@doc)
+    get_input
+    get_book
   end
 
   def self.scrape_cat2 #Harcover Nonfiction
-    doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/new-york-times-bestsellers-hardcover-nonfiction/_/N-1p5q"))
-    top_ten(doc)
-    puts ""
-    puts "Please type the number of the book you would like more information on:"
-    input = gets.strip
-    book = input.to_i
-
-    url = "http://www.barnesandnoble.com#{doc.css("#listView > li:nth-child(#{book}) > ul > li > div.product-image > a:nth-child(1)").attribute("href").value}"
-    book_info = Nokogiri::HTML(open("#{url}"))
-    title = book_info.css("#prodSummary > h1").text
-    author = book_info.css("#prodSummary > span > a").text
-    summary = book_info.css("#productInfoOverview > div > div > p:nth-child(3)").text
-    puts ""
-    puts "-------------------------------------------------------"
-    puts "TITLE:       #{title}"
-    puts "AUTHOR:      #{author}"
-    puts ""
-    puts "SYNOPSIS:    #{summary}"
+    @doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/new-york-times-bestsellers-hardcover-nonfiction/_/N-1p5q"))
+    top_ten(@doc)
+    get_input
+    get_book
   end
 
   def self.scrape_cat3 #Paperback Fiction
-    doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/new-york-times-bestsellers-trade-paperback-fiction/_/N-1p3v"))
-    top_ten(doc)
-    puts ""
-    puts "Please type the number of the book you would like more information on:"
-    input = gets.strip
-    book = input.to_i
-
-    url = "http://www.barnesandnoble.com#{doc.css("#listView > li:nth-child(#{book}) > ul > li > div.product-image > a:nth-child(1)").attribute("href").value}"
-    book_info = Nokogiri::HTML(open("#{url}"))
-    title = book_info.css("#prodSummary > h1").text
-    author = book_info.css("#prodSummary > span > a").text
-    summary = book_info.css("#productInfoOverview > div > div > p:nth-child(3)").text
-    puts ""
-    puts "-------------------------------------------------------"
-    puts "TITLE:       #{title}"
-    puts "AUTHOR:      #{author}"
-    puts ""
-    puts "SYNOPSIS:    #{summary}"
+    @doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/new-york-times-bestsellers-trade-paperback-fiction/_/N-1p3v"))
+    top_ten(@doc)
+    get_input
+    get_book
   end
 
   def self.scrape_cat4 #Paperback Nonfiction
-    doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/new-york-times-bestsellers-paperback-nonfiction/_/N-1p3u"))
-    top_ten(doc)
-    puts ""
-    puts "Please type the number of the book you would like more information on:"
-    input = gets.strip
-    book = input.to_i
-
-    url = "http://www.barnesandnoble.com#{doc.css("#listView > li:nth-child(#{book}) > ul > li > div.product-image > a:nth-child(1)").attribute("href").value}"
-    book_info = Nokogiri::HTML(open("#{url}"))
-    title = book_info.css("#prodSummary > h1").text
-    author = book_info.css("#prodSummary > span > a").text
-    summary = book_info.css("#productInfoOverview > div > div > p:nth-child(3)").text
-    puts ""
-    puts "-------------------------------------------------------"
-    puts "TITLE:       #{title}"
-    puts "AUTHOR:      #{author}"
-    puts ""
-    puts "SYNOPSIS:    #{summary}"
+    @doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/new-york-times-bestsellers-paperback-nonfiction/_/N-1p3u"))
+    top_ten(@doc)
+    get_input
+    get_book
   end
 
   def self.scrape_cat5 #Advice and How-To
-    doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/new-york-times-bestsellers-advice-how-to-miscellaneous/_/N-1p3o"))
-    top_ten(doc)
-    puts ""
-    puts "Please type the number of the book you would like more information on:"
-    input = gets.strip
-    book = input.to_i
-
-    url = "http://www.barnesandnoble.com#{doc.css("#listView > li:nth-child(#{book}) > ul > li > div.product-image > a:nth-child(1)").attribute("href").value}"
-    book_info = Nokogiri::HTML(open("#{url}"))
-    title = book_info.css("#prodSummary > h1").text
-    author = book_info.css("#prodSummary > span > a").text
-    summary = book_info.css("#productInfoOverview > div > div > p:nth-child(3)").text
-    puts ""
-    puts "-------------------------------------------------------"
-    puts "TITLE:       #{title}"
-    puts "AUTHOR:      #{author}"
-    puts ""
-    puts "SYNOPSIS:    #{summary}"
+    @doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/new-york-times-bestsellers-advice-how-to-miscellaneous/_/N-1p3o"))
+    top_ten(@doc)
+    get_input
+    get_book
   end
 
 
