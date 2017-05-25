636fa74d9b3d1237f9e19d5247940080f8a8c410
hyoyou <heatherhyou@gmail.com>
2017-05-25 13:15:42 -0400

    Created class method that displays top 10 books in each category

--- lib/nyt_bestseller/chart.rb
+++ lib/nyt_bestseller/chart.rb
@@ -1,76 +1,40 @@
 class NytBestseller::Chart
-  #come back to refactor this code
-  def self.scrape_cat1
+
+  def self.top_ten(doc)
+    name = doc.css(".html-embed-container").first.text.strip
+    puts ""
+    puts "Top 10 Books in #{name}:"
+
+    i = 1
+    while i < 11
+      listview = doc.css("#listView > li:nth-child(#{i}) > ul > li > div.product-info")
+      puts "     #{i}. " + "#{listview.css("p.product-info-title > a").text} by: " + "#{listview.css("span > a").first.text}"
+      i += 1
+    end
+  end
+
+  def self.scrape_cat1 #Hardcover Fiction
     doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/new-york-times-bestsellers-hardcover-fiction/_/N-1p3r"))
-
-    name = doc.css(".html-embed-container").first.text.strip
-    puts ""
-    puts "Top 15 Books in #{name}:"
-
-    i = 1
-    while i < 16
-      listview = doc.css("#listView > li:nth-child(#{i}) > ul > li > div.product-info")
-      puts "#{i}. " + "#{listview.css("p.product-info-title > a").text}     by: " + "#{listview.css("span > a").first.text}"
-      i += 1
-    end
+    top_ten(doc)
   end
 
-  def self.scrape_cat2
+  def self.scrape_cat2 #Harcover Nonfiction
     doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/new-york-times-bestsellers-hardcover-nonfiction/_/N-1p5q"))
-
-    name = doc.css(".html-embed-container").first.text.strip
-    puts ""
-    puts "Top 15 Books in #{name}:"
-
-    i = 1
-    while i < 16
-      listview = doc.css("#listView > li:nth-child(#{i}) > ul > li > div.product-info")
-      puts "#{i}. " + "#{listview.css("p.product-info-title > a").text}     by: " + "#{listview.css("span > a").first.text}"
-      i += 1
-    end
+    top_ten(doc)
   end
 
-  def self.scrape_cat3
+  def self.scrape_cat3 #Paperback Fiction
     doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/new-york-times-bestsellers-trade-paperback-fiction/_/N-1p3v"))
-
-    name = doc.css(".html-embed-container").first.text.strip
-    puts ""
-    puts "Top 10 Books in #{name}:"
-
-    i = 1
-    while i < 11
-      listview = doc.css("#listView > li:nth-child(#{i}) > ul > li > div.product-info")
-      puts "#{i}. " + "#{listview.css("p.product-info-title > a").text}     by: " + "#{listview.css("span > a").first.text}"
-      i += 1
-    end
+    top_ten(doc)
   end
 
-  def self.scrape_cat4
+  def self.scrape_cat4 #Paperback Nonfiction
     doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/new-york-times-bestsellers-paperback-nonfiction/_/N-1p3u"))
-
-    name = doc.css(".html-embed-container").first.text.strip
-    puts ""
-    puts "Top 10 Books in #{name}:"
-
-    i = 1
-    while i < 11
-      listview = doc.css("#listView > li:nth-child(#{i}) > ul > li > div.product-info")
-      puts "#{i}. " + "#{listview.css("p.product-info-title > a").text}     by: " + "#{listview.css("span > a").first.text}"
-      i += 1
-    end
+    top_ten(doc)
   end
 
-  def self.scrape_cat5
+  def self.scrape_cat5 #Advice and How-To
     doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/new-york-times-bestsellers-advice-how-to-miscellaneous/_/N-1p3o"))
-
-    name = doc.css(".html-embed-container").first.text.strip
-    puts "Top 10 Books in #{name}:"
-
-    i = 1
-    while i < 11
-      listview = doc.css("#listView > li:nth-child(#{i}) > ul > li > div.product-info")
-      puts "#{i}. " + "#{listview.css("p.product-info-title > a").text}     by: " + "#{listview.css("span > a").first.text}"
-      i += 1
-    end
+    top_ten(doc)
   end
 end
