97f61ba547a531f08112585c458c3e1bb4cb2fee
hyoyou <heatherhyou@gmail.com>
2017-05-30 12:42:42 -0400

    Added Book and Category directory to environment

--- lib/nyt_bestseller.rb
+++ lib/nyt_bestseller.rb
@@ -5,3 +5,5 @@ require 'pry'
 require_relative "./nyt_bestseller/version"
 require_relative "./nyt_bestseller/cli"
 require_relative "./nyt_bestseller/scraper"
+require_relative "./nyt_bestseller/category"
+require_relative "./nyt_bestseller/book"
